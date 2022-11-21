34a8610603a32e19c1666a9ece7f177b2b36ea79
jamesrave <117628362+jamesrave@users.noreply.github.com>
2022-11-21 10:20:08 +0300

    Add files via upload

---
+++ dkr 2 1.pas
@@ -0,0 +1,17 @@
+﻿const rave=11;
+var a:array of integer;h:real;
+begin
+setlength(a,rave);
+  for var i:=1 to 5 do
+  begin
+    a[i]:=random(1,10);
+    print(a[i],' ')
+  end;
+  println;
+  h:=0;
+  for var i:=1 to 5 do 
+  begin
+    h:=h+a[i]/5;
+  end;
+  println('среднерифмитческое= ',h);
+  end.
---
+++ dkr 2 2.pas
@@ -0,0 +1,29 @@
+﻿begin
+var a:=ReadString('Введите строку:');
+var b:=ReadString('Введите подстроку:');
+var s:integer;
+var x:string;
+var h:=0;
+var i:=1;
+var k:=0;
+for var n:=1 to length(b) do
+a:=a+' ';
+while i<length(a) do begin
+for var j:=1 to length(b) do begin
+if a[i+j-1]=b[j] then s:=1 else begin s:=0; break; end;
+end;
+if s=1 then begin h:=h+1; i:=i+length(b); end
+else i:=i+1;
+end;
+i:=1;
+while i<length(a) do begin
+for var j:=1 to length(b) do begin
+if a[i+j-1]=b[j] then s:=1 else begin s:=0; break; end;
+end;
+if s=1 then k:=k+1;
+if (s=1) and ((k=1) or (k=h)) then i:=i+length(b)
+else begin x:=x+a[i]; i:=i+1;
+end;
+end;
+println(x);
+end.
